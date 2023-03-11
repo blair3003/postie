1153e21e660c0f2242087edaedda0652ad36ed64
Blair Fleming <blair3003.bf@gmail.com>
2023-03-11 09:54:10 +0000

    comps

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,27 @@
+const Header = () => {
+    return (
+        <header>
+            <h1>Postie</h1>
+        </header>
+    )
+}
+
+const Main = () => {
+    return (
+        <main>
+            <p>The main content</p>
+        </main>
+    )
+}
+
+
 function App() {
-    return <div className="app"></div>
+    return (
+        <>
+            <Header />
+            <Main />
+        </>
+    )
 }
 
 export default App
