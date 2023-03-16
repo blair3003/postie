5cdf08e5455683609114976ca11e8940717a7e55
Blair Fleming <blair3003.bf@gmail.com>
2023-03-16 10:48:34 +0000

    Nav

---
+++ src/components/Nav.jsx
@@ -0,0 +1,12 @@
+const Nav = ({ className }) => {
+
+    return (
+        <nav className={className}>
+            <a href="#">Posts</a>
+            <a href="#">Users</a>
+            <a href="#">Other</a>
+        </nav>
+    )
+}
+
+export default Nav
--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -1,14 +1,22 @@
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
-import Menu from '../components/Menu'
+import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai'
+import Nav from '../components/Nav'
 
 const Header = () => {
 
+    const [toggle, setToggle] = useState(false)
+
     return (
-        <header className="bg-red-700 p-4 flex justify-between items-center">
+        <header className="bg-red-700 p-4 flex justify-between items-center relative">
             <Link to="/">
                 <h1 className="uppercase text-white text-3xl font-montserrat">Postie</h1>
             </Link>
-            <Menu />
+            <button onClick={() => setToggle(toggle => toggle ? false : true)} className="md:hidden">
+                {toggle ? <AiOutlineClose /> : <AiOutlineMenu />}
+            </button>
+            <Nav className="hidden md:flex gap-4" />
+            {toggle ? <Nav className="md:hidden flex flex-col gap-4 absolute top-full" /> : null}
         </header>
     )
 }
