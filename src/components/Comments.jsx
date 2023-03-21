18cc689f56fcdab9997949fcafe048e10f904f5a
Blair Fleming <blair3003.bf@gmail.com>
2023-03-21 10:17:03 +0000

    sorted comments

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -2,11 +2,25 @@ import Comment from './Comment'
 
 const Comments = ({ comments }) => {
 
+    // Sort comments by age and depth
+    const sorted = []
+    comments.sort((a, b) => Date.parse(a.createdAt) > Date.parse(b.createdAt))
+    comments.forEach(comment => {
+        if (comment.parent) {
+            let index = sorted.findIndex(parent => parent._id === comment.parent)
+            sorted.splice(++index, 0, comment)
+        } else sorted.unshift(comment)
+    })
+
+
+
+
+
     return (
         <ol>
-        {comments ? comments.map(comment =>
-            <Comment comment={comment} />
-        ) : null}
+        {sorted.map(comment =>
+            <Comment key={comment._id} comment={comment} />
+        )}
         </ol>
     )
 }
