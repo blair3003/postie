b6d1d7f8696312595375bb56bbaf8c2299fac8f2
Blair Fleming <blair3003.bf@gmail.com>
2023-04-17 11:29:45 +0100

    preview sizing

--- src/features/posts/PostPreview.jsx
+++ src/features/posts/PostPreview.jsx
@@ -3,16 +3,16 @@ import { Link } from 'react-router-dom'
 const PostPreview = ({ post }) => {
 
     return (
-        <article className="">
-            <Link to={`/posts/${post._id}`}>
-                <div className="bg-white p-4 rounded-lg">
-                    <img className="w-full" src={post.thumbnail ? post.thumbnail : 'img/default-thumbnail.png'} alt={post.title} />
+        <article className="bg-white p-4 rounded-lg m-4 shadow">
+            <Link to={`/posts/${post._id}`} className="">
+                <div className="overflow-hidden aspect-video">
+                    <img className="rounded-lg" src={post.thumbnail ? post.thumbnail : 'img/default-thumbnail.png'} alt={post.title} />
                 </div>
-                <div className="flex gap-4 p-4">
+                <div className="flex gap-4 mt-4">
                     <div className="w-12">
                         <img className="rounded-full drop-shadow" src={post.author.pic ? post.author.pic : 'img/default-pic.png'} alt={post.author.name} />
                     </div>
-                    <div>
+                    <div className="grow">
                         <h3 className="text-lg font-bold">{post.title}</h3>
                         <p>{post.author.name}</p>
                         <p>{post.createdAt}</p>
