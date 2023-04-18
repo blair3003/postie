93f646291dfa80da94a73d0a495715b9dc42c36e
Blair Fleming <blair3003.bf@gmail.com>
2023-04-18 11:29:46 +0100

    Update Footer.jsx

--- src/layouts/Footer.jsx
+++ src/layouts/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
     return (
-        <footer className="grid place-content-center bg-slate-900 p-4">
+        <footer className="grid place-content-center bg-slate-800 p-4">
             <p className="text-white">Postie &copy; {new Date().getFullYear()}</p>
         </footer>
     )
