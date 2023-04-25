c4c4062ae697dff2e693520146c73e280bd8fc43
Blair Fleming <blair3003.bf@gmail.com>
2023-04-25 10:20:38 +0100

    Update Missing.jsx

--- src/layouts/Missing.jsx
+++ src/layouts/Missing.jsx
@@ -6,7 +6,7 @@ const Missing = () => {
 		<article className="max-w-2xl mx-auto bg-white/50 p-4 mb-8 rounded-lg shadow">
 
 			<h1 className="text-2xl font-pacifico p-2">Page missing</h1>
-			<p className="p-2">The page you are trying to acces doesn't exist...</p>
+			<p className="p-2">The page you are trying to access doesn't exist...</p>
 			<Link to="/" className="font-bold underline p-2">Back to home</Link>
 
 		</article>
