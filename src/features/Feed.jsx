9fb3beb7cfa5ad5b6cef50bd0df5804df086791f
Blair Fleming <blair3003.bf@gmail.com>
2023-05-16 12:27:32 +0100

    Update Feed.jsx

--- src/features/Feed.jsx
+++ src/features/Feed.jsx
@@ -27,7 +27,7 @@ const Feed = () => {
 
     return (
         error ? <p>Error loading feed!</p> :
-        loading ? <><Loading /> Please allow up to a minute for the Render.com server to wake up.</> :
+        loading ? <><Loading /> Please allow a few minutes for the Render.com server to wake up.</> :
         
         <section id="feed">
             <h1 className="offscreen">Feed</h1>
